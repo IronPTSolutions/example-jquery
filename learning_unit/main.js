b7d5934f7852d4ceea99bcbf024af7c86af22a60
Julio García Pérez <juliojose.garcia@11paths.com>
2017-11-14 20:46:41 +0100

    add steps

--- learning_unit/main.js
+++ learning_unit/main.js
@@ -1,11 +1,25 @@
 // Tasks
 
-// 1. Add the “selected” class to the first element of the menú
+$(document).ready(function() {
 
-// 2. Add the “middle” class to the middle articles in the <section> tags
+  // 1. Add the “selected” class to the first element of the menu
 
-// 3. Add the “highlighted” class to the .list-item even elements
+  $("#menu li:first").addClass("selected");
 
-// 4. Add a “1px solid #eee” border to the last .list-item element
+  // 2. Add the “middle” class to the middle articles in the <section> tags
 
-// 5. In the form, change the border color of the focused input
+  $("section article:not(:first-child):not(:last-child)").addClass("middle")
+
+  // 3. Add the “highlighted” class to the .list-item even elements
+
+  $(".list-item:even").addClass("highlighted");
+
+  // 4. Add a “1px solid #eee” border to the last .list-item element
+
+  $(".list-item:last").css("border", "1px solid red");
+
+  // 5. In the form, change the border color of the focused input
+
+  $("form input:focus").css("border", "1px solid red");
+
+});
